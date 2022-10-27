5455df38dce5a77cf429b8ae416bf4dd2e5a621b
Nekotato1412 <nekotato1412@gmail.com>
2022-10-27 09:08:41 -0500

    Added basic left-right camera follow back in

--- Game.gd
+++ Game.gd
@@ -2,9 +2,14 @@ extends Spatial
 
 onready var TimeRemaining = $HUD/TimeRemaining
 onready var GameTime = $"Game Time"
+onready var Camera = $Camera
+onready var Player = $Player
 
 func _on_Game_Time_timeout():
 	pass # Replace with function body.
+
+func _physics_process(delta):
+	Camera.translation.x = Player.translation.x
 
 # warning-ignore:unused_argument
 func _process(delta):
