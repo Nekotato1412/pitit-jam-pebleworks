f547203c2d34edcd4d8f1d0f1dc7b3edea134fbf
Nekotato1412 <nekotato1412@gmail.com>
2022-10-27 09:05:32 -0500

    Added basic raycast

--- Player.gd
+++ Player.gd
@@ -9,17 +9,23 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("move_right"):
 		velocity.x -= 1
+		
 	if Input.is_action_pressed("move_left"):
 		velocity.x += 1
+
 	if Input.is_action_pressed("move_down"):
 		velocity.z -= 1
+		
 	if Input.is_action_pressed("move_up"):
 		velocity.z += 1
-	
+		
 	velocity = velocity.normalized()
 	
 	if Input.is_action_pressed("sprint"):
 		velocity = velocity * sprintMultiplier
+		
+	if velocity:
+		look_at(transform.origin + velocity, Vector3.UP)
 	
 # warning-ignore:return_value_discarded
 	move_and_slide(velocity * speed)
